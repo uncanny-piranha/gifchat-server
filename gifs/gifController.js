79c97004b31e2a5b5fd852e6cf0ab679f6d51340
Mike <mikeh09@gmail.com>
2014-10-22 15:33:10 -0700

    checks mongo for gifs, otherwise makes call to giffy and stores results

--- gifs/gifController.js
+++ gifs/gifController.js
@@ -1,4 +1,6 @@
 var Gif = require('./gifModel.js');
+var Q = require('q');
+var request = require('request');
 
 module.exports = {
   retrieveGif: function(req, res, next){
@@ -13,16 +15,39 @@ module.exports = {
             var randIndex = Math.floor(Math.random()*gif.urls.length);
             res.json({url: gif.urls[randIndex]});
           } else {
-            //do something(send back random gif?)
+            //SENDS BACK SAME GIF EACH TIME NONE IS FOUND. MIGHT WANT TO RANDOMIZE THIS
+            res.json({url: 'http://i.imgur.com/PVSpM9X.gif'});
           }
         } else {
         //else, hit giffy api
-          //if results sent back, send back random url from results, store results in mongo
-          //else do something(send back random gif?) and store the (empty) results in mongo
+          request.get({url: 'http://api.giphy.com/v1/gifs/search?q='+keyword+'&api_key=dc6zaTOxFJmzC&limit=100', json: true}, function (err, httpRes, body) {
+            if (err) {
+              return console.log('Error fetching data from giphy: ' + err);
+            }
+            //if results sent back, send back random url from results
+            if (body.data.length > 0) {
+              var randIndex = Math.floor(Math.random()*body.data.length);
+              res.json({url: body.data[randIndex].images.original.url});
+            } else {
+              //SENDS BACK SAME GIF EACH TIME NONE IS FOUND. MIGHT WANT TO RANDOMIZE THIS
+              res.json({url: 'http://i.imgur.com/PVSpM9X.gif'});
+            }
+            //Store results in mongo
+            var gifArray = [];
+            for(var i = 0; i < body.data.length; i++){
+              gifArray.push(body.data[i].images.original.url);
+            }
+            createGif = Q.nbind(Gif.create, Gif);
+            newGif = {
+              keyword: keyword,
+              urls: gifArray
+            };
+            return createGif(newGif);
+          });
         }
       })
       .fail(function (error) {
         next(error);
-      })
+      });
   }
 };
